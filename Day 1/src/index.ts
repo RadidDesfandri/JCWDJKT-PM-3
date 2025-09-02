4d74075ee44e13f21edc7ef62f1df8f60d95761f
RadidDesafandri <radietdesfandri@gmail.com>
2025-09-02 20:46:01 +0700

    Day 1

---
+++ Day 1/src/index.ts
@@ -0,0 +1,63 @@
+import express, { Response, Request, Application } from "express";
+
+const PORT = 8000;
+
+const app: Application = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).send("Welcome");
+});
+
+// GET, POST, PUT, PATCH, DELETE
+
+// Method GET -> Untuk mengambil data
+app.get("/api", (req: Request, res: Response) => {
+  return res.status(200).send({
+    status: 200,
+    msg: "Response untuk method GET",
+  });
+});
+
+// Method POST -> Untuk membuat data
+app.post("/api", (req: Request, res: Response) => {
+  return res.json({
+    status: 200,
+    msg: `Response untuk method GET, Dengan body ${JSON.stringify(req.body)}`,
+  });
+});
+
+// Method PUT -> Untuk memperbarui seluruh data
+app.put("/api/:id", (req: Request, res: Response) => {
+  return res.json({
+    status: 200,
+    msg: `Response untuk method PUT, Dengan body ${JSON.stringify(
+      req.body
+    )} dan ID: ${req.params.id}`,
+  });
+});
+
+// Method PATCH -> Untuk memperbarui sebagian data
+app.patch("/api/:id", (req: Request, res: Response) => {
+  return res.json({
+    status: 200,
+    msg: `Response untuk method PATCH, Dengan body ${JSON.stringify(
+      req.body
+    )} dan ID: ${req.params.id}`,
+  });
+});
+
+// Menggunakan function terpisah
+const deleteData = (req: Request, res: Response) => {
+  return res.json({
+    status: 200,
+    msg: `Response untuk method DELETE sebagai function, Dengan ID: ${req.params.id}`,
+  });
+};
+
+// Method DELETE -> Untuk menghapus data
+app.delete("/api/:id", deleteData);
+
+app.listen(PORT, () => {
+  console.log(`Application run on => http://localhost:${PORT}`);
+});
